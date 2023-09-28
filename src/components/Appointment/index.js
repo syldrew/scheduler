ac50c9898e10d7ac6b752ea7d67c71f5a0a2c2a1
syldrew <syl_peke14@hotmail.com>
2023-09-28 15:23:37 -0500

    Add Appointment and Button test

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,119 +1,112 @@
 import React from "react";
-
 import "components/Appointment/styles.scss";
+import Header from "components/Appointment/Header";
 
-import Header from "./Header";
-import Show from "./Show";
-import Empty from "./Empty";
-import Status from "./Status";
+import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
 import Form from "./Form";
+import Status from "./Status";
 import Confirm from "./Confirm";
 import Error from "./Error";
+
 import useVisualMode from "hooks/useVisualMode";
 
-
-const EMPTY = "EMPTY";
-const SHOW = "SHOW";
-const CREATE = "CREATE";
-const STATUS = "STATUS";
-const CONFIRM = "CONFIRM";
-const EDIT = "EDIT";
-const ERROR_SAVE = "ERROR_SAVE";
-const ERROR_DELETE = "ERROR_DELETE";
-
-
 export default function Appointment(props) {
-    const { mode, transition, back } = useVisualMode(
-      props.interview ? SHOW : EMPTY
-    );
-  
-    function save(name, interviewer) {
-      const interview = {
-        student: name,
-        interviewer,
-      };
-      transition(STATUS);
-      props
-        .bookInterview(props.id, interview)
-        .then(() => {
-          transition(SHOW);
-        })
-        .catch((error) => {
-          transition(ERROR_SAVE, true);
-        });
-    }
-  
-    function destroy() {
-      transition(STATUS);
-      props
-        .cancelInterview(props.id)
-        .then(() => {
-          console.log("hi");
-          transition(EMPTY);
-        })
-        .catch(() => {
-          transition(ERROR_DELETE, true);
-        });
-    }
-  
-    const onDelete = () => {
-      transition(CONFIRM);
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+  const SAVING = "SAVING";
+  const DELETING = "DELETING";
+  const ERROR_SAVE = "ERROR_SAVE";
+  const ERROR_DELETE = "ERROR_DELETE";
+  const EDIT = "EDIT";
+  const CONFIRM = "CONFIRM";
+
+
+
+  function save(name, interviewer) {
+    const interview = {
+      student: name,
+      interviewer
     };
-    const onCancel = () => {
-      transition(SHOW);
-    };
-    const onEdit = () => {
-      transition(EDIT);
-    };
-  
-    return (
-      <article className="appointment">
-        {props.time ? <Header time={props.time} /> : "No Appointments"}
+
+    transition(SAVING);
+
+    props
+      .bookInterview(props.id, interview)
+      .then(() => transition(SHOW))
+      .catch(error => transition(ERROR_SAVE, true));
+  }
+
+  function destroy() {
+    transition(DELETING, true);
+    props
+      .cancelInterview(props.id)
+      .then(() => transition(EMPTY))
+      .catch(error => transition(ERROR_DELETE, true));
+  }
+
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
+
+  return (
+
+    <article className="appointment" data-testid="appointment">
+      <Header time={props.time} />
+      {props.interview ?
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+          onDelete={() => console.log("CONFIRM")}
+          onEdit={() => console.log("EDIT")}
+        />
+        :
+        <Empty onAdd={() => console.log("CREATE")} />}
         {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
-        {mode === SHOW && props.interview && (
-          <Show
-            student={props.interview.student}
+
+        {mode === SHOW &&
+          <Show student={props.interview.student}
             interviewer={props.interview.interviewer}
-            onDelete={onDelete}
-            onEdit={onEdit}
+            onDelete={() => transition(CONFIRM)}
+            onEdit={() => transition(EDIT)}
           />
-        )}
+        }
+
         {mode === CREATE && (
-          <Form
-            onCancel={() => {
-              transition(EMPTY);
-            }}
-            onSave={save}
-            interviewers={props.interviewers}
-          />
+          <Form interviewers={props.interviewers} onCancel={back} onSave={save} />
         )}
+
         {mode === EDIT && (
           <Form
-            onCancel={() => {
-              transition(SHOW);
-            }}
-            onSave={save}
+            name={props.interview.student}
+            interviewer={props.interview.interviewer.id}
             interviewers={props.interviewers}
-            student={props.interview.student}
-            interviewer={props.interview.interviewer}
+            onCancel={back}
+            onSave={save}
           />
         )}
-        {mode === ERROR_SAVE && <Error onClose={back} />}
-        {mode === ERROR_DELETE && (
-          <Error
-            onClose={() => {
-              transition(SHOW);
-            }}
-          />
-        )}
-        {mode === STATUS && <Status />}
+        {mode === SAVING && <Status message="Saving" />}
+
+        {mode === DELETING && <Status message="Deleting" />}
+
         {mode === CONFIRM && (
           <Confirm
+            message="Are you sure you would like to delete?"
+            onCancel={back}
             onConfirm={destroy}
-            onCancel={onCancel}
-            message="Are you sure you'd like to delete?"
           />
         )}
-      </article>
-    );
-  }
+
+        {mode === ERROR_SAVE && (
+          <Error message="Could not book appointment." onClose={back} />
+        )}
+
+        {mode === ERROR_DELETE && (
+          <Error message="Could not cancel appointment." onClose={back} />
+        )}
+
+    </article>
+  );
+}
---
+++ src/components/__test__/Button.test.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import Button from 'components/Button';
+
+
+it("renders without crashing", () => {
+  render(<Button />);
+});
+
+
+it("renders its `children` prop as text", () => {
+  const { getByText } = render(<Button>Default</Button>);
+  expect(getByText("Default")).toBeInTheDocument();
+});
